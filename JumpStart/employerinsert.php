0e66977c83d2b8e9dbbd0e9cde12c31373d93150
Hyrkan <109449003+Hyrkhan@users.noreply.github.com>
2023-12-12 15:29:49 +0800

    Update employerinsert.php

--- JumpStart/employerinsert.php
+++ JumpStart/employerinsert.php
@@ -75,7 +75,7 @@ if (isset($_POST['login_user'])) {
   	if (mysqli_num_rows($results) == 1) {
   	  $_SESSION['email'] = $email;
   	  $_SESSION['success'] = "You are now logged in";
-  	  header('location: employerHomepage.php');
+  	  header('location: ../JumpStart/Employer_Website/employerHomepage.html');
   	}else {
   		array_push($errors, "Wrong email/password combination");
   	}
