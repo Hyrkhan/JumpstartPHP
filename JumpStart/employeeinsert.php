bb35e43f2a1af61c328009e0de38f53ce8af8ece
Hyrkan <109449003+Hyrkhan@users.noreply.github.com>
2023-12-12 15:29:07 +0800

    Update employeeinsert.php

--- JumpStart/employeeinsert.php
+++ JumpStart/employeeinsert.php
@@ -5,53 +5,70 @@ session_start();
 $errors = array(); 
 
 // connect to the database
-$db = mysqli_connect('localhost:3307', 'root', '', 'jumpstartdb');
+$db = mysqli_connect('localhost', 'root', '', 'jumpstartdb');
 
 // REGISTER USER
 if (isset($_POST['reg_user'])) {
-    // receive all input values from the form
-    $email = mysqli_real_escape_string($db, $_POST['email']);
-    $password = mysqli_real_escape_string($db, $_POST['password']);
-    $confirmpassword = mysqli_real_escape_string($db, $_POST['confirmpassword']);
+  // receive all input values from the form
+  $email = mysqli_real_escape_string($db, $_POST['email']);
+  $password = mysqli_real_escape_string($db, $_POST['password']);
+  $confirmpassword = mysqli_real_escape_string($db, $_POST['confirmpassword']);
 
-    // form validation: ensure that the form is correctly filled ...
-    // by adding (array_push()) corresponding error unto $errors array
-    if (empty($email)) { array_push($errors, "Email is required"); }
-    if (empty($password)) { array_push($errors, "Password is required"); }
+  // form validation: ensure that the form is correctly filled ...
+  // by adding (array_push()) corresponding error unto $errors array
+  if (empty($email)) { array_push($errors, "Email is required"); }
+  if (empty($password)) { array_push($errors, "Password is required"); }
+  if ($password != $confirmpassword) {
+    array_push($errors, "The two passwords do not match");
+  }
 
-    // Validate using JavaScript
-    echo '<script src="myscripts.js"></script>';
-    echo '<script type="text/javascript">validateForm(event);</script>';
+  // first check the database to make sure 
+  // a user does not already exist with the same email
+  $user_check_query = "SELECT * FROM employeeregistration WHERE email='$email' LIMIT 1";
+  $result = mysqli_query($db, $user_check_query);
+  $user = mysqli_fetch_assoc($result);
 
-    // Check if JavaScript validation passed
-    if (count($errors) == 0) {
-        // first check the database to make sure 
-        // a user does not already exist with the same email
-        $user_check_query = "SELECT * FROM employeeregistration WHERE email='$email' LIMIT 1";
-        $result = mysqli_query($db, $user_check_query);
-        $user = mysqli_fetch_assoc($result);
+  if ($user) {
+    array_push($errors, "Email already exists");
+  }
 
-        if ($user) {
-            array_push($errors, "Email already exists");
-        }
+  // Finally, register user if there are no errors in the form
+  if (count($errors) == 0) {
+    // encrypt the password before saving in the database
+    $password = md5($password);
 
-        // Finally, register user if there are no errors in the form
-        if (count($errors) == 0) {
-            // encrypt the password before saving in the database
-            $password = md5($password);
+    $query = "INSERT INTO employeeregistration (email, password) 
+              VALUES('$email', '$password')";
+    mysqli_query($db, $query);
+    $_SESSION['email'] = $email;
+    $_SESSION['success'] = "You are now registered and logged in";
+    header('location: EmployeeLogin.php');
+  }
+}
 
-            $query = "INSERT INTO employeeregistration (email, password) 
-                    VALUES('$email', '$password')";
-                    
-            if (mysqli_query($db, $query)) {
-                $_SESSION['email'] = $email;
-                $_SESSION['success'] = "You are now registered and logged in";
-                echo '<script>window.location.href = "index.php";</script>';
-                exit();
-            } else {
-                echo "Error: " . $query . "<br>" . mysqli_error($db);
-            }
-        }
-    }
+// LOGIN EMPLOYEE
+if (isset($_POST['login_user'])) {
+  $email = mysqli_real_escape_string($db, $_POST['email']);
+  $password = mysqli_real_escape_string($db, $_POST['password']);
+
+  if (empty($email)) {
+  	array_push($errors, "Email is required");
+  }
+  if (empty($password)) {
+  	array_push($errors, "Password is required");
+  }
+
+  if (count($errors) == 0) {
+  	$password = md5($password);
+  	$query = "SELECT * FROM employeeregistration WHERE email='$email' AND password='$password'";
+  	$results = mysqli_query($db, $query);
+  	if (mysqli_num_rows($results) == 1) {
+  	  $_SESSION['email'] = $email;
+  	  $_SESSION['success'] = "You are now logged in";
+  	  header('location: employeeHomepage.php');
+  	}else {
+  		array_push($errors, "Wrong email/password combination");
+  	}
+  }
 }
 ?>
